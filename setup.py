1f691797b1126b593c99c23491a75e8db1fbed29
Onur Erkin Sucu <onurerkinsucu@gmail.com>
2020-05-18 09:59:41 -0400

    cookiecutter project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='McKinsey ProHack DS Competition',
+    author='Onur',
+    license='MIT',
+)
